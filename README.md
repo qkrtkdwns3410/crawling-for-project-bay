524002b40f94bb84877a7fe9970e3d0f078fdeb6
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-11-16 00:03:19 +0900

    feat: dotenv를 통한 이메일 비밀번호 설정 및 로깅 추가

--- README.md
+++ README.md
@@ -8,5 +8,15 @@ pip install -r requirements.txt
 
 ### .env 파일 로컬 커밋 대상에서 지우는 방법
 ```
+git update-index --assume-unchanged <env경로>
+```
+
+### .env 파일 로컬 커밋 대상에 추가하는 방법
+```
 git update-index --no-assume-unchanged <env경로>
+```
+
+### 포함되지 않도록 한 파일 목록을 보는 방법
+```
+git ls-files -v | grep '^h'
 ```
--- crawling/jobplanet/JobPlanet.py
+++ crawling/jobplanet/JobPlanet.py
@@ -1,11 +1,24 @@
-"""
- *packageName    : 
- * fileName       : JobPlanet
- * author         : ipeac
- * date           : 24. 11. 15.
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 24. 11. 15.        ipeac       최초 생성
-"""
+import logging
+import os
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+
+import crawling.logging.logging_config as logging_config
+
+# 로깅 설정
+logging_config.setup_logging()
+logger = logging.getLogger(__name__)
+
+# .env 파일 로드
+load_dotenv()
+
+# 이메일 비밀번호 호출
+EMAIL = os.getenv('EMAIL')
+PASSWORD = os.getenv('PASSWORD')
+
+logger.info(f'EMAIL, PASSWORD: {EMAIL}, {PASSWORD}')
+
+# chromeDriver 위치설정
